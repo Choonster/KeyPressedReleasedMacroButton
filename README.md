e1bfc419a1cea6e6d6d448e118624471733a9dcf
Choonster <Choonster.2010@gmail.com>
2014-12-13 01:38:46 +1100

    Initial commit (version 1.0)

---
+++ README.md
@@ -0,0 +1,8 @@
+# Installation
+* Download the AddOn by clicking the "Download ZIP" button
+* Extract the downloaded ZIP
+* Rename the innermost folder (containing the AddOn files) to `KeyPressedReleasedMacroButton`
+* Copy that folder into your **WoW\Interface\AddOns** folder
+
+# Usage
+Open core.lua in a text editor (one with Lua syntax highlighting is preferred, but not required) and change the macros at the top of the file (inside of `[==[` and `]==]`) to whatever you want to run when the key is pressed and released.
---
+++ core.lua
@@ -0,0 +1,23 @@
+-- The macro to use when the key is pressed
+local DOWN_MACRO = [[
+/use Burning Rush
+]]
+
+-- The macro to use when the key is released
+local UP_MACRO = [[
+/stopcasting [channelling]
+]]
+
+-------------------
+-- END OF CONFIG --
+-------------------
+
+_G["BINDING_HEADER_KeyPressedReleasedMacroButton"] = "Key Pressed/Released Macro Button"
+_G["BINDING_NAME_CLICK KeyPressedReleasedMacroButton"] = "Use Button"
+
+local button = CreateFrame("Button", "KeyPressedReleasedMacroButton", nil, "SecureActionButtonTemplate")
+button:RegisterForClicks("AnyUp", "AnyDown")
+button:SetAttribute("downbutton", "Button31") -- Remap to button 31 on down clicks, use left button (button 1) on up clicks
+button:SetAttribute("type", "macro")
+button:SetAttribute("macrotext31", DOWN_MACRO)
+button:SetAttribute("macrotext1", UP_MACRO)
